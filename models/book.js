ce742a6b4ec9d1869ace892296c603f5bde9d20c
Christian Fuchs <christian.fuchs5@gmx.at>
2020-07-24 11:00:30 +0200

    init project
    add dependencies and set up sequelize and the DB

---
+++ models/book.js
@@ -0,0 +1,55 @@
+const Sequelize = require('sequelize');
+ 
+module.exports = (sequelize) => {
+ 
+    class Book extends Sequelize.Model {}
+    Book.init({
+
+        title:{
+            type: Sequelize.STRING,
+            validate:{
+                notEmpty:{
+                    msg: 'The "TITLE" can not be empty'
+                }
+            }
+        },
+
+        author:{
+            type: Sequelize.STRING,
+            validate:{
+                notEmpty:{
+                    msg: 'The "AUTHOR" can not be empty'
+                }
+            }
+        },
+
+        genre:{
+            type: Sequelize.STRING,
+            validate:{
+                notEmpty:{
+                    msg: 'The "GENRE" can not be empty'
+                }
+            }
+        },
+
+        year:{
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            validate:{
+                notNull:{
+                    msg: 'Please provide a value for the year'
+                },
+                is: /^\d{4}$/
+            }
+        },
+
+        
+
+
+
+
+    }, {sequelize});
+ 
+    return Book;
+ 
+};
